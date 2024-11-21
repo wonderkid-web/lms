2bad09c12cc54387541c6abeecfb8d01d28a6cc5
Muhammad Wahyu Ramadhan <mwahyuap2018@gmail.com>
2024-11-21 09:02:39 +0700

    ui: adding icon & web name

--- src/component/layout/SignoutButton.tsx
+++ src/component/layout/SignoutButton.tsx
@@ -5,10 +5,10 @@ import { signOut } from "next-auth/react";
 function SignoutButton() {
   return (
     <button
-      className="ml-auto flex items-center justify-center rounded-sm bg-red-500 px-4 py-1 text-sm text-white"
+      className="ml-auto flex items-center justify-center rounded-sm bg-red-600 px-4 py-1 text-sm text-white"
       onClick={async () => await signOut()}
     >
-      Signout
+      keluar
     </button>
   );
 }
